a13b1a14c06b37a2ee3baa85de681aefba9cdfa9
James Mead <james@floehopper.org>
2011-05-23 18:07:56 +0100

    Create a list for the event and add attendees as members of the list.
    Note that calling Twitter.list_create for a list that already exists doesn't create a duplicate list nor raise an exception. And calling Twitter.list_add_members for members that are already in the list doesn't add duplicate members nor raise an exception.
    
    Note also you will need to add the real credentials in a config.yml file in the same format as config.example.yml.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source :rubygems
+
+gem "mechanize"
+gem "nokogiri"
+gem "twitter"
--- scrape.rb
+++ scrape.rb
@@ -1,6 +1,7 @@
 require "rubygems"
+require "bundler/setup"
+
 require "mechanize"
-require "nokogiri"
 
 class NokogiriParser < Mechanize::Page
   attr_reader :doc
@@ -10,10 +11,28 @@ class NokogiriParser < Mechanize::Page
   end
 end
 
+event_attendees_url = "http://lanyrd.com/2011/euruko/attendees"
+event_name = "euruko-2011"
+
+attendees = []
 agent = Mechanize.new
 agent.pluggable_parser.html = NokogiriParser
-agent.get("http://lanyrd.com/2011/euruko/attendees") do |page|
+agent.get(event_attendees_url) do |page|
   (page/"ul.people li span:not(.name)").each do |span|
-    puts span.inner_text.strip
+    attendees << span.inner_text.strip[1..-1]
   end
 end
+
+require "twitter"
+
+config = YAML.load_file("config.yml")
+
+Twitter.configure do |twitter|
+  twitter.consumer_key = config[:consumer_key]
+  twitter.consumer_secret = config[:consumer_secret]
+  twitter.oauth_token = config[:oauth_token]
+  twitter.oauth_token_secret = config[:oauth_token_secret]
+end
+
+Twitter.list_create(event_name)
+Twitter.list_add_members(event_name, attendees)
