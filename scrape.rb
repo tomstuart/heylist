85cfb7d68637594b12dccfcd83ef6c619b858dda
James Adam <james@lazyatom.com>
2011-05-24 11:23:50 +0100

    Encapsulate the behaviour into methods.

--- scrape.rb
+++ scrape.rb
@@ -2,6 +2,7 @@ require "rubygems"
 require "bundler/setup"
 
 require "mechanize"
+require "twitter"
 
 class NokogiriParser < Mechanize::Page
   attr_reader :doc
@@ -10,20 +11,6 @@ class NokogiriParser < Mechanize::Page
     super(uri, response, body, code)
   end
 end
-
-event_attendees_url = "http://lanyrd.com/2011/euruko/attendees"
-event_name = "euruko-2011"
-
-attendees = []
-agent = Mechanize.new
-agent.pluggable_parser.html = NokogiriParser
-agent.get(event_attendees_url) do |page|
-  (page/"ul.people li span:not(.name)").each do |span|
-    attendees << span.inner_text.strip[1..-1]
-  end
-end
-
-require "twitter"
 
 config = YAML.load_file("config.yml")
 
@@ -34,5 +21,35 @@ Twitter.configure do |twitter|
   twitter.oauth_token_secret = config[:oauth_token_secret]
 end
 
-Twitter.list_create(event_name)
-Twitter.list_add_members(event_name, attendees)
+
+
+def attendees_for_event(url)
+  attendees = []
+  agent = Mechanize.new
+  agent.pluggable_parser.html = NokogiriParser
+  agent.get(url) do |page|
+    (page/"ul.people li span:not(.name)").each do |span|
+      attendees << span.inner_text.strip[1..-1]
+    end
+  end
+  attendees
+end
+
+def ensure_list(name)
+  puts "Ensuring list %p exists" % name
+  Twitter.list(name)
+rescue Twitter::NotFound
+  Twitter.list_create(name)
+end
+
+def ensure_list_members(name, attendees)
+  puts "Ensuring #{attendees.length} attendees for list %p" % name
+  Twitter.list_add_members(name, attendees)
+end
+
+
+event_attendees_url = "http://lanyrd.com/2011/euruko/attendees"
+event_name = "euruko-2011"
+
+ensure_list(event_name)
+ensure_list_members(event_name, attendees_for_event(event_attendees_url))
